a70cefdaeb0ed042efd8ad9f50120946975cabf3
vherrera04 <vherrera9975.vh@gmail.com>
2024-09-02 11:20:03 -0500

    Update HW3.cu

--- HW3.cu
+++ HW3.cu
@@ -1,4 +1,4 @@
-//nvcc bouncingBallsInABoxBroken.cu -o bounce -lglut -lm -lGLU -lGL																													
+//nvcc HW3.cu -o myHW3 -lglut -lm -lGLU -lGL																													
 //To stop hit "control c" in the window you launched it from.
 #include <iostream>
 #include <fstream>
