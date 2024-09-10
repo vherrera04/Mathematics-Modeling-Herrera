1c57e88117b3b5bb68e0c45031b4dd6df0ae8492
vherrera04 <vherrera9975.vh@gmail.com>
2024-09-10 10:35:11 -0500

    Update HW6.cu

--- HW6.cu
+++ HW6.cu
@@ -1,4 +1,4 @@
-//nvcc AsteriodsLocalDampingBroken.cu -o bounce -lglut -lm -lGLU -lGL																													
+//nvcc HW6.cu -o bounce -lglut -lm -lGLU -lGL																													
 //To stop hit "control c" in the window you launched it from.
 #include <iostream>
 #include <fstream>
